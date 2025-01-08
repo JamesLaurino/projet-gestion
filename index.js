20f893ec0ecdda34b6c12f13cbd330acf74ae9ae
JamesLaurino <157245042+JamesLaurino@users.noreply.github.com>
2025-01-08 20:20:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use('/', router);
 
-const port = process.env.PORT || 3001
+const port = 3000
 
 db.connect().then(
     app.listen(port, () => {
